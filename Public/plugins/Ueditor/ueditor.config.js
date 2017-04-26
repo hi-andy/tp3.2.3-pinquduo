66aa54b2d7b94072d14ae37c359115ae2eb42b1b
yonghua <yonghua.me@outlook.com>
2017-04-26 18:00:13 +0800

    restore edit goods detail page

--- Public/plugins/Ueditor/ueditor.config.js
+++ Public/plugins/Ueditor/ueditor.config.js
@@ -179,18 +179,63 @@
         , getMovieUrl: URL_getMovie                  //视频数据获取地址
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
+        // , toolbars: [
+        //     ['fullscreen', 'source', '|', 'undo', 'redo', '|',
+        //         'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+        //         'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+        //         'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+        //         'directionalityltr', 'directionalityrtl', 'indent', '|',
+        //         'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
+        //         'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+        //         'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
+        //         'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
+        //         'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', '|',
+        //         'print', 'preview', 'searchreplace', 'help']
+        // ]
+        //,
+        ,zIndex: 999,
+        // initialFrameWidth: "65%", //初化宽度
+         initialFrameHeight: 400, //初化高度
+        focus: false, //初始化时，是否让编辑器获得焦点true或false
+        maximumWords: 99999,
+        removeFormatAttributes: 'class,style,lang,width,height,align,hspace,valign',
+         //允许的最大字符数 'fullscreen',
+        pasteplain: true,
+        autoHeightEnabled: true,
+        autotypeset: {
+            mergeEmptyline: true,        //合并空行
+            removeClass: true,           //去掉冗余的class
+            removeEmptyline: false,      //去掉空行
+            textAlign: "left",           //段落的排版方式，可以是 left,right,center,justify 去掉这个属性表示不执行排版
+            imageBlockLine: 'center',    //图片的浮动方式，独占一行剧中,左右浮动，默认: center,left,right,none 去掉这个属性表示不执行排版
+            pasteFilter: false,          //根据规则过滤没事粘贴进来的内容
+            clearFontSize: false,        //去掉所有的内嵌字号，使用编辑器默认的字号
+            clearFontFamily: false,      //去掉所有的内嵌字体，使用编辑器默认的字体
+            removeEmptyNode: false,      // 去掉空节点
+                                         //可以去掉的标签
+            removeTagNames: {"font": 1},
+            indent: false,               // 行首缩进
+            indentValue: '0em'
+        }
         , toolbars: [
-            ['fullscreen', 'source', '|', 'undo', 'redo', '|',
-                'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+            ['fullscreen', 'source', '|', 'undo', 'redo',
+                '|', 'bold', 'italic', 'underline', 'fontborder',
+                'strikethrough', 'superscript', 'subscript',
+                'removeformat', 'formatmatch', 'autotypeset',
+                'blockquote', 'pasteplain', '|', 'forecolor',
+                'backcolor', 'insertorderedlist',
+                'insertunorderedlist', 'selectall', 'cleardoc', '|',
                 'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
-                'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
-                'directionalityltr', 'directionalityrtl', 'indent', '|',
-                'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
-                'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
-                'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
-                'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
-                'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', '|',
-                'print', 'preview', 'searchreplace', 'help']
+                'customstyle', 'paragraph', 'fontfamily', 'fontsize',
+                '|', 'directionalityltr', 'directionalityrtl',
+                'indent', '|', 'justifyleft', 'justifycenter',
+                'justifyright', 'justifyjustify', '|', 'touppercase',
+                'tolowercase', '|', 'link', 'unlink', '|',
+                'imagenone', 'imageleft', 'imageright', 'imagecenter',
+                '|', 'insertimage', 'emotion',
+                 'pagebreak', 'template',
+                'background', '|', 'horizontal',
+                 '|', 'print', 'preview', 'searchreplace']
         ]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         , labelMap: {
