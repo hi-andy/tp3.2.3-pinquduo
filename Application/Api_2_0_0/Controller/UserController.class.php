ac260fa823f070879cbb9bd3480e0ee4395ba4dd
王子 <mengzhuowei@qq.com>
2017-05-10 18:25:16 +0800

    修改

--- Application/Api_2_0_0/Controller/UserController.class.php
+++ Application/Api_2_0_0/Controller/UserController.class.php
@@ -2071,7 +2071,7 @@ class UserController extends BaseController {
         $type = I('type',0);//0.全部 1.拼团中 2.待发货 3.待收货 4.待付款 5.已完成
         $page = I('page',1);
         $pagesize = I('pagesiaze',20);
-        $rdsname = "getOrderList".$user_id.$type.$page.$pagesize;
+        $rdsname = "getOrderList_".$user_id.$type.$page.$pagesize;
 
         if(empty(redis($rdsname))) {//判断是否有缓存
             if ($type == 1) {
