8657e07d224d0d97bd7a71ad01f812123dfe3709
王子 <mengzhuowei@qq.com>
2017-05-15 16:53:25 +0800

    商品详情增加下架判断

--- Application/Api_2_0_0/Controller/GoodsController.class.php
+++ Application/Api_2_0_0/Controller/GoodsController.class.php
@@ -1085,59 +1085,71 @@ class GoodsController extends BaseController {
             }
         }
         //////
-        $rdsname = 'getDetaile_'.$goods_id;
-		if(empty(redis($rdsname))){
-			//轮播图
-			$banner = M('goods_images', '', 'DB_CONFIG2')->where("`goods_id` = $goods_id")->field('image_url')->select();
+        $goodsstatus = M('goods')
+            ->where(array(
+                'goods_id'=>array('eq',$goods_id),
+                'show_type'=>array('eq',1),
+                'is_show'=>array('eq',0),
+                'is_on_sale'=>array('eq',0)
+            ))
+            ->count();
+        if ($goodsstatus >0){
+            $json = array('status' => -1, 'msg' => '该商品已下架', 'result' => '');
+        } else {
+            $rdsname = 'getDetaile_' . $goods_id;
+            if (empty(redis($rdsname))) {
+                //轮播图
+                $banner = M('goods_images', '', 'DB_CONFIG2')->where("`goods_id` = $goods_id")->field('image_url')->select();
 
-			foreach ($banner as &$v) {
-				//TODO 缩略图处理
-				$v['small'] = TransformationImgurl($v['image_url']);
-				$v['origin'] = TransformationImgurl($v['image_url']);
-				unset($v['image_url']);
-			}
+                foreach ($banner as &$v) {
+                    //TODO 缩略图处理
+                    $v['small'] = TransformationImgurl($v['image_url']);
+                    $v['origin'] = TransformationImgurl($v['image_url']);
+                    unset($v['image_url']);
+                }
 
-			if (empty($banner)) {
-				$banner = null;
-			}
-			$goods = $this->getGoodsInfo($goods_id);
-			//商品规格
-			$goodsLogic = new \Home\Logic\GoodsLogic();
-			$spec_goods_price = M('spec_goods_price', '', 'DB_CONFIG2')->where("goods_id = $goods_id")->select(); // 规格 对应 价格 库存表
-			$filter_spec = $goodsLogic->get_spec($goods_id);//规格参数
-			$new_spec_goods = array();
-			foreach ($spec_goods_price as $spec) {
-				$new_spec_goods[] = $spec;
-			}
-			$new_filter_spec = array();
+                if (empty($banner)) {
+                    $banner = null;
+                }
+                $goods = $this->getGoodsInfo($goods_id);
+                //商品规格
+                $goodsLogic = new \Home\Logic\GoodsLogic();
+                $spec_goods_price = M('spec_goods_price', '', 'DB_CONFIG2')->where("goods_id = $goods_id")->select(); // 规格 对应 价格 库存表
+                $filter_spec = $goodsLogic->get_spec($goods_id);//规格参数
+                $new_spec_goods = array();
+                foreach ($spec_goods_price as $spec) {
+                    $new_spec_goods[] = $spec;
+                }
+                $new_filter_spec = array();
 
-			foreach ($filter_spec as $key => $filter) {
-				$new_filter_spec[] = array('title' => $key, 'items' => $filter);
-			}
-			for ($i = 0; $i < count($new_filter_spec); $i++) {
-				foreach ($new_filter_spec[$i]['items'] as &$v) {
-					if (!empty($v['src'])) {
-						$v['src'] =$v['src'];
-					}
-				}
-			}
-			//如果有传规格过来就改变商品名字
-			if (!empty($spec_key)) {
-				$key_name = M('spec_goods_price', '', 'DB_CONFIG2')->where("`key`='$spec_key'")->field('key_name')->find();
-				$goods['goods_spec_name'] = $goods['goods_name'] . $key_name['key_name'];
-			}
-			if (!empty($ajax_get)) {
-				$goods['html'] = htmlspecialchars_decode($goods['goods_content']);
-			}
+                foreach ($filter_spec as $key => $filter) {
+                    $new_filter_spec[] = array('title' => $key, 'items' => $filter);
+                }
+                for ($i = 0; $i < count($new_filter_spec); $i++) {
+                    foreach ($new_filter_spec[$i]['items'] as &$v) {
+                        if (!empty($v['src'])) {
+                            $v['src'] = $v['src'];
+                        }
+                    }
+                }
+                //如果有传规格过来就改变商品名字
+                if (!empty($spec_key)) {
+                    $key_name = M('spec_goods_price', '', 'DB_CONFIG2')->where("`key`='$spec_key'")->field('key_name')->find();
+                    $goods['goods_spec_name'] = $goods['goods_name'] . $key_name['key_name'];
+                }
+                if (!empty($ajax_get)) {
+                    $goods['html'] = htmlspecialchars_decode($goods['goods_content']);
+                }
 
-			//提供保障
-			$security = array(array('type'=>'全场包邮','desc'=>'所有商品均无条件包邮'),array('type'=>'7天退换','desc'=>'商家承诺7天无理由退换货'),array('type'=>'48小时发货','desc'=>'成团后，商家将在48小时内发货'),array('type'=>'假一赔十','desc'=>'若收到的商品是假货，可获得加倍赔偿'));
+                //提供保障
+                $security = array(array('type' => '全场包邮', 'desc' => '所有商品均无条件包邮'), array('type' => '7天退换', 'desc' => '商家承诺7天无理由退换货'), array('type' => '48小时发货', 'desc' => '成团后，商家将在48小时内发货'), array('type' => '假一赔十', 'desc' => '若收到的商品是假货，可获得加倍赔偿'));
 
-			$json = array('status' => 1, 'msg' => '获取成功', 'result' => array('banner' => $banner, 'goods_id' => $goods['goods_id'], 'goods_name' => $goods['goods_name'], 'prom_price' => $goods['prom_price'], 'market_price' => $goods['market_price'], 'shop_price' => $goods['shop_price'], 'prom' => $goods['prom'], 'goods_remark' => $goods['goods_remark'], 'store_id' => $goods['store_id'] , 'is_support_buy' => $goods['is_support_buy'], 'is_special' => $goods['is_special'], 'original_img' => $goods['original_img'], 'goods_content_url' => $goods['goods_content_url'], 'goods_share_url' => $goods['goods_share_url'], 'fenxiang_url' => $goods['fenxiang_url'], 'collect' => $goods['collect'],'original_img'=>$goods['original_img'],'img_arr'=>$goods['img_arr'],'security'=>$security,'store' => $goods['store'],  'spec_goods_price' => $new_spec_goods, 'filter_spec' => $new_filter_spec));
-			redis($rdsname, serialize($json));//写入缓
-		}else{
-			$json = unserialize(redis($rdsname));
-		}
+                $json = array('status' => 1, 'msg' => '获取成功', 'result' => array('banner' => $banner, 'goods_id' => $goods['goods_id'], 'goods_name' => $goods['goods_name'], 'prom_price' => $goods['prom_price'], 'market_price' => $goods['market_price'], 'shop_price' => $goods['shop_price'], 'prom' => $goods['prom'], 'goods_remark' => $goods['goods_remark'], 'store_id' => $goods['store_id'], 'is_support_buy' => $goods['is_support_buy'], 'is_special' => $goods['is_special'], 'original_img' => $goods['original_img'], 'goods_content_url' => $goods['goods_content_url'], 'goods_share_url' => $goods['goods_share_url'], 'fenxiang_url' => $goods['fenxiang_url'], 'collect' => $goods['collect'], 'original_img' => $goods['original_img'], 'img_arr' => $goods['img_arr'], 'security' => $security, 'store' => $goods['store'], 'spec_goods_price' => $new_spec_goods, 'filter_spec' => $new_filter_spec));
+                redis($rdsname, serialize($json));//写入缓
+            } else {
+                $json = unserialize(redis($rdsname));
+            }
+        }
 		I('ajax_get') && $ajax_get = I('ajax_get');//网页端获取数据标示
 		if(!empty($ajax_get))
 			$this->getJsonp($json);
