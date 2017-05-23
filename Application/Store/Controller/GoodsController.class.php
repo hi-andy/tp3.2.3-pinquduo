7db83c5b527304c2788d10ecae36ea9d55dc04ba
jiang949676565 <949676565@qq.com>
2017-05-23 17:28:39 +0800

    BUG修复

--- Application/Store/Controller/GoodsController.class.php
+++ Application/Store/Controller/GoodsController.class.php
@@ -382,9 +382,7 @@ class GoodsController extends BaseController {
                     }
                     $Goods->save(); // 写入数据到数据库
                     $Goods->afterSave($goods_id);
-                }
-                else
-                {
+                }else{
                     $Goods->is_on_sale = 0 ;    // 默认
                     $Goods->is_show = 0 ;
                     $goods_id = $insert_id = $Goods->add(); // 写入数据到数据库
