297df4f0704a8ab018ba6eda15f2c314c80d4555
jiang949676565 <949676565@qq.com>
2017-05-25 16:59:14 +0800

    增加活动小logo

--- Application/Api_2_0_1/Controller/IndexController.class.php
+++ Application/Api_2_0_1/Controller/IndexController.class.php
@@ -50,6 +50,8 @@ class IndexController extends BaseController {
             //中间活动模块
             $activity['banner_url'] = CDN . '/Public/images/daojishibanner.jpg';
             $activity['H5_url'] = 'http://pinquduo.cn/index.php?s=/Api/SecondBuy/';
+            $activity['logo_url'] = 'http://cdn.pinquduo.cn/0d1miaosha.gif';
+            
             $where = '`show_type`=0 and `is_show` = 1 and `is_on_sale` = 1 and `is_recommend`=1 and `is_special` in (0,1) and `is_audit`=1 ';
             $result2 = $this->getGoodsList($where,$page,$pagesize,'is_recommend desc,sort asc');
             $json = array('status' => 1, 'msg' => '获取成功', 'result' => array('goodsList' => $result2, 'activity' => $activity, 'ad' => $data, 'cat' => $category));
