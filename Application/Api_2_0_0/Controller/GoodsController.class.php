5bec7ea36a5cb8cbbf0b3fd96f2a5ba1ddec79f8
王子 <mengzhuowei@qq.com>
2017-05-25 16:49:13 +0800

    搜索索引

--- Application/Api_2_0_0/Controller/GoodsController.class.php
+++ Application/Api_2_0_0/Controller/GoodsController.class.php
@@ -874,12 +874,7 @@ class GoodsController extends BaseController {
         $pagesize = I('pagesize',50);
         $rdsname = "getsearch".$key.$page.$pagesize;
         if (empty(redis($rdsname))) {//判断是否有缓存
-            $key = str_split_utf8($key);
-            $where = "";
-            for ($i=0; $i < count($key); $i++) {
-                $where .= "`goods_name` like '%{$key[$i]}%' and ";
-            }
-            $where .= "`is_show`=1 and `is_on_sale`=1 and `is_audit`=1 and `show_type`=0 ";
+            $where = "`goods_name` like '%{$key}%' and `is_show`=1 and `is_on_sale`=1 and `is_audit`=1 and `show_type`=0 ";
             $data = $this->getGoodsList($where,$page,$pagesize);
             $json = array('status' => 1, 'msg' => '获取成功', 'result' => $data);
             redis($rdsname, serialize($json), REDISTIME);//写入缓存
