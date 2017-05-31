ec02d5e69538e6f6f9a250fe2bbf2b769df2d6d8
王子 <mengzhuowei@qq.com>
2017-05-31 10:36:35 +0800

    免单拼缓存名称增加版本号

--- Application/Api_2_0_1/Controller/IndexController.class.php
+++ Application/Api_2_0_1/Controller/IndexController.class.php
@@ -561,7 +561,8 @@ class IndexController extends BaseController {
     {
         $page = I('page',1);
         $pagesize = I('pagesize',10);
-        $rdsname = "getFreeProm201".$page.$pagesize;
+        $version = I('version','');
+        $rdsname = "getFreeProm201".$version.$page.$pagesize;
         if(empty(redis($rdsname))) {//判断是否有缓存
             $where = '`show_type`=0 and `is_special`=6 and `is_on_sale`=1 and `is_show`=1 and `is_audit`=1 ';
             $data = $this->getGoodsList($where,$page,$pagesize,'is_recommend desc,sort asc');
