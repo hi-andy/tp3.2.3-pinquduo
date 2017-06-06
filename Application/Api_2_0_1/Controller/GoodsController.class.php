33b2062a78837e2439a1ba8812d32ecf1e0bb7db
王子 <mengzhuowei@qq.com>
2017-06-06 17:41:19 +0800

    搜索

--- Application/Api_2_0_1/Controller/GoodsController.class.php
+++ Application/Api_2_0_1/Controller/GoodsController.class.php
@@ -866,7 +866,7 @@ class GoodsController extends BaseController {
             $sc = new \SphinxClient(); // 实例化Api
             $sc->setServer('39.108.12.198', 9312); // 设置服务端，第一个参数sphinx服务器地址，第二个sphinx监听端口
             $res = $sc->query($key, 'test1'); // 执行查询，第一个参数查询的关键字，第二个查询的索引名称，mysql索引名称（这个也是在配置文件中定义的），多个索引名称以,分开，也可以用*表示所有索引。
-            print_r($res);
+            var_dump($res);
             exit;
         }
         $page = I('page',1);
