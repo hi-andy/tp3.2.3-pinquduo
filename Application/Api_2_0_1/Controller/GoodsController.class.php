c9356ae692d09abe4b7b38201f4d0ca5e1604d85
王子 <mengzhuowei@qq.com>
2017-06-06 19:00:55 +0800

    搜索优化

--- Application/Api_2_0_1/Controller/GoodsController.class.php
+++ Application/Api_2_0_1/Controller/GoodsController.class.php
@@ -865,8 +865,14 @@ class GoodsController extends BaseController {
             vendor('sphinx.sphinxapi');
             $sc = new \SphinxClient(); // 实例化Api
             $sc->setServer('39.108.12.198', 9312); // 设置服务端，第一个参数sphinx服务器地址，第二个sphinx监听端口
-            $res = $sc->query($key, 'test1'); // 执行查询，第一个参数查询的关键字，第二个查询的索引名称，mysql索引名称（这个也是在配置文件中定义的），多个索引名称以,分开，也可以用*表示所有索引。
-            var_dump($res);
+            $res = (array) $sc->query($key, 'test1'); // 执行查询，第一个参数查询的关键字，第二个查询的索引名称，mysql索引名称（这个也是在配置文件中定义的），多个索引名称以,分开，也可以用*表示所有索引。
+            $ids = '';
+            foreach ($res['matches'] as $k => $v){
+                $ids .= $k.',';
+            }
+            $ids = substr($ids, 0, -1);
+            $goods = M('goods')->where("goods_id in({$ids})")->select();
+            echo json_encode($goods);
             exit;
         }
         $page = I('page',1);
