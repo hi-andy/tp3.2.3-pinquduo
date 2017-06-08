6f48763494c30466402b0b57b33111d8d639bcd7
jiang949676565 <949676565@qq.com>
2017-06-08 16:58:37 +0800

    海淘商品保存失败问题

--- Application/Admin/Model/GoodsModel.class.php
+++ Application/Admin/Model/GoodsModel.class.php
@@ -19,107 +19,143 @@ class GoodsModel extends Model {
         array('prom','require','团购人数必须填写！',1,'',3),
         array('shop_price','/\d{1,10}(\.\d{1,2})?$/','本店售价格式不对。',2,'regex'),
         array('prom','/\d{1,10}(\.\d{1,2})?$/','开团人数格式不对。',2,'regex')
-     );   
-    
-    
-    
+     );
+
+
+
     /**
      * 后置操作方法
      * 自定义的一个函数 用于数据保存后做的相应处理操作, 使用时手动调用
      * @param int $goods_id 商品id
      */
     public function afterSave($goods_id)
-    {            
-         // 商品货号
-         $goods_sn = "TP".str_pad($goods_id,7,"0",STR_PAD_LEFT);   
-         $this->where("goods_id = $goods_id and goods_sn = ''")->save(array("goods_sn"=>$goods_sn)); // 根据条件更新记录
+    {
+        // 商品货号
+        $goods_sn = "TP".str_pad($goods_id,7,"0",STR_PAD_LEFT);
+        $this->where("goods_id = $goods_id and goods_sn = ''")->save(array("goods_sn"=>$goods_sn)); // 根据条件更新记录
 
-        $goods = M('goods')->where("goods_id = $goods_id")->find();
-        if($_POST['original_img']!=$goods['original_img'])
+        // 商品图片相册  图册
+        if(count($_POST['goods_images']) > 1)
         {
-            $link =  C('HTTP_URL').goods_thum_images($_POST['goods_id'],400,400);
-            $res = unlink($link);
-            $link1 = C('HTTP_URL').$goods['original_img'];
-            $res1 = unlink($link1);
+            $goodsImagesArr = M('GoodsImages')->where("goods_id = $goods_id")->getField('img_id,image_url'); // 查出所有已经存在的图片
+
+            // 删除图片
+            foreach($goodsImagesArr as $key => $val)
+            {
+                if(!in_array($val, $_POST['goods_images']))
+                    M('GoodsImages')->where("img_id = {$key}")->delete(); // 删除所有状态为0的用户数据
+            }
+            // 添加图片
+            foreach($_POST['goods_images'] as $key => $val)
+            {
+                if($val == null)  continue;
+                if(!in_array($val, $goodsImagesArr))
+                {
+                    $data = array(
+                        'goods_id' => $goods_id,
+                        'image_url' => $val,
+                    );
+                    M("GoodsImages")->data($data)->add();; // 实例化User对象
+                }
+            }
         }
 
-         // 商品图片相册  图册
-         if(count($_POST['goods_images']) > 1)
-         {             
-//             array_unshift($_POST['goods_images'],$_POST['original_img']); // 商品原始图 默认为 相册第一张图片
-//             array_pop($_POST['goods_images']); // 弹出最后一个
-             $goodsImagesArr = M('GoodsImages')->where("goods_id = $goods_id")->getField('img_id,image_url'); // 查出所有已经存在的图片
-             
-             // 删除图片
-             foreach($goodsImagesArr as $key => $val)
-             {
-                 if(!in_array($val, $_POST['goods_images']))
-                     M('GoodsImages')->where("img_id = {$key}")->delete(); // 删除所有状态为0的用户数据
-             }
-             // 添加图片
-             foreach($_POST['goods_images'] as $key => $val)
-             {
-                 if($val == null)  continue;                                  
-                 if(!in_array($val, $goodsImagesArr))
-                 {                 
-                        $data = array(
-                            'goods_id' => $goods_id,
-                            'image_url' => $val,
-                        );
-                        M("GoodsImages")->data($data)->add();; // 实例化User对象                     
-                 }
-             }
-         }
-         
-         // 商品规格价钱处理
-         if($_POST['item'])
-         {
-//             $spec = M('Spec')->getField('id,name'); // 规格表
-//             $specItem = M('SpecItem')->getField('id,item');//规格项
-                          
-             $specGoodsPrice = M("SpecGoodsPrice"); // 实例化 商品规格 价格对象
-             $specGoodsPrice->where('goods_id = '.$goods_id)->delete(); // 删除原有的价格规格对象
-             foreach($_POST['item'] as $k => $v)
-             {
-                   // 批量添加数据
-                   $v['price'] = trim($v['price']);
-                   $store_count = $v['store_count'] = trim($v['store_count']); // 记录商品总库存
-                   $v['bar_code'] = trim($v['bar_code']);
-                   $dataList[] = array('goods_id'=>$goods_id,'key'=>$k,'key_name'=>$v['key_name'],'price'=>$v['price'],'prom_price'=>$v['prom_price'],'store_count'=>$v['store_count'],'bar_code'=>$v['bar_code']);
-             }
-             //规格名称
-             $keys = array_keys($_POST['item']);
-             for($i=0;$i<count($_POST['item']);$i++)
-             {
-                 $key_word = explode('_',$keys[$i]);
-                 $key_name = null;
-                 for($j=0;$j<count($key_word);$j++)
-                 {
-                     $Item= M('SpecItem')->where('`id`='.$key_word[$j])->find();
-                     $spec = M('spec')->where('`id`='.$Item['spec_id'])->find();
-                     $name[] = $spec['name'].':'.$Item['item'].' ';
-                 }
-                 for($z=0;$z<count($name);$z++)
-                 {
-                     $key_name = $key_name.$name[$z];
-                 }
-                 $dataList[$i]['key_name'] = $key_name;
-                 $name=null;
-             }
+        // 商品规格价钱处理
+        if($_POST['item'])
+        {
+            $specGoodsPrice = M("SpecGoodsPrice"); // 实例化 商品规格 价格对象
+            $specGoodsPrice->where('goods_id = '.$goods_id)->delete(); // 删除原有的价格规格对象
 
-             $specGoodsPrice->addAll($dataList);             
-             //M('Goods')->where("goods_id = 1")->save(array('store_count'=>10)); // 修改总库存为各种规格的库存相加
-         }
+            //将上商品的规格价格调试为APP选项的价格
+            //筛选出最低的价格
+            //如果勾选了批量填写就将某个价格锁定不进行筛选了
+            $min_num = key($_POST['item']);
+            $min_prom = $_POST['item'][$min_num]['prom_price'];
+            $min_show = $_POST['item'][$min_num]['price'];
 
-         // 商品规格图片处理
-         if($_POST['item_img'])
-         {    
-             M('SpecImage')->where("goods_id = $goods_id")->delete(); // 把原来是删除再重新插入
-             foreach ($_POST['item_img'] as $key => $val)
-             {                 
-                 M('SpecImage')->data(array('goods_id'=>$goods_id ,'spec_image_id'=>$key,'src'=>$val))->add();
-             }                                                    
-         }
-         refresh_stock($goods_id); // 刷新商品库存
+            if($_POST['price']=='on')
+            {
+                foreach($_POST['item'] as $k=>$v)
+                {
+                    $_POST['item'][$k]['price'] = $min_show;
+                }
+                $d['shop_price'] = $min_show;
+            }else{
+                foreach($_POST['item'] as $k=>$v)
+                {
+                    if($v['price']<$min_show){
+                        $min_show=$v['price'];
+                    }
+                }
+                $d['shop_price'] = $min_show;
+            }
+            if($_POST['prom_price']=='on')
+            {
+                foreach($_POST['item'] as $k=>$v)
+                {
+                    $_POST['item'][$k]['prom_price'] =$min_prom;
+                }
+                $d['prom_price'] = $min_prom;
+            }else{
+                foreach($_POST['item'] as $k=>$v)
+                {
+                    if($v['prom_price']<$min_prom){
+                        $min_prom=$v['prom_price'];
+                    }
+                }
+                $d['prom_price'] = $min_prom;
+            }
+            if($_POST['count']=='on')
+            {
+                $count = $_POST['item'][$min_num]['store_count'];
+                foreach($_POST['item'] as $k=>$v)
+                {
+                    $_POST['item'][$k]['store_count'] =$count;
+                }
+            }
+
+            foreach($_POST['item'] as $k => $v)
+            {
+                // 批量添加数据
+                $v['price'] = trim($v['price']);
+                $v['prom_price'] = trim($v['prom_price']);
+                $v['store_count'] = trim($v['store_count']); // 记录商品总库存
+                $dataList[] = array('goods_id'=>$goods_id,'key'=>$k,'key_name'=>$v['key_name'],'price'=>$v['price'],'prom_price'=>$v['prom_price'],'store_count'=>$v['store_count']);
+            }
+            //规格名称
+            $keys = array_keys($_POST['item']);
+            for($i=0;$i<count($_POST['item']);$i++)
+            {
+                $key_word = explode('_',$keys[$i]);
+                $key_name = null;
+                for($j=0;$j<count($key_word);$j++)
+                {
+                    $Item= M('SpecItem')->where('`id`='.$key_word[$j])->find();
+                    $spec = M('spec')->where('`id`='.$Item['spec_id'])->find();
+                    $name[] = $spec['name'].':'.$Item['item'].' ';
+                }
+                for($z=0;$z<count($name);$z++)
+                {
+                    $key_name = $key_name.$name[$z];
+                }
+                $dataList[$i]['key_name'] = $key_name;
+                $name=null;
+            }
+
+            $specGoodsPrice->addAll($dataList);
+            //M('Goods')->where("goods_id = 1")->save(array('store_count'=>10)); // 修改总库存为各种规格的库存相加
+        }
+
+        $res = M('goods')->where('goods_id='.$goods_id)->data($d)->save();
+        // 商品规格图片处理
+        if($_POST['item_img'])
+        {
+            M('SpecImage')->where("goods_id = $goods_id")->delete(); // 把原来是删除再重新插入
+            foreach ($_POST['item_img'] as $key => $val)
+            {
+                M('SpecImage')->data(array('goods_id'=>$goods_id ,'spec_image_id'=>$key,'src'=>$val))->add();
+            }
+        }
+        refresh_stock($goods_id); // 刷新商品库存
     }
 }
