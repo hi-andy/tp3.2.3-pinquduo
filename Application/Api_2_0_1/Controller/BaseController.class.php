91948b994b2beb04d85752e62a0af4a785962312
王子 <mengzhuowei@qq.com>
2017-06-09 09:44:24 +0800

    8小时自动成团

--- Application/Api_2_0_1/Controller/BaseController.class.php
+++ Application/Api_2_0_1/Controller/BaseController.class.php
@@ -844,9 +844,7 @@ class BaseController extends Controller {
      */
     public function get_robot($not_in_user_id='') {
         if (!empty($not_in_user_id)) {
-            $user = M('','','DB_CONFIG2')->query("SELECT user_id FROM `tp_users`
-WHERE user_id <> {$not_in_user_id} AND test=1 AND user_id <> {$not_in_user_id} and user_id >= (SELECT floor( RAND() * ((SELECT MAX(user_id) FROM `tp_users`)-(SELECT MIN(user_id) FROM `tp_users`)) + (SELECT MIN(user_id) FROM `tp_users`))) 
-ORDER BY user_id LIMIT 1");
+            $user = M('','','DB_CONFIG2')->query("select user_id from tp_users order by rand() LIMIT 1");
             return $user[0];
         }
     }
