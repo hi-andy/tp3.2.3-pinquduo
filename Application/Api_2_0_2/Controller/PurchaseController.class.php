0d6837dfa0b371e1f135dec95485e4e0293ba610
timeing <641227924@qq.com>
2017-07-14 10:15:44 +0800

    qq公众号上的支付宝支付问题

--- Application/Api_2_0_2/Controller/PurchaseController.class.php
+++ Application/Api_2_0_2/Controller/PurchaseController.class.php
@@ -475,7 +475,7 @@ class PurchaseController extends  BaseController
                     $this->aftermath($user_id,$goods,$num,$o_id);//修改库存
                 }
                 if(!empty($ajax_get)){
-                    echo "<script> location.href='http://wx.pinquduo.cn/order_detail.html?order_id='+$o_id+'&type=2&user_id='+$user_id </script>";
+//注释掉代码  温立涛                    echo "<script> location.href='http://wx.pinquduo.cn/order_detail.html?order_id='+$o_id+'&type=2&user_id='+$user_id </script>";
                     exit;
                 }
                 exit(json_encode($json));
@@ -756,7 +756,7 @@ class PurchaseController extends  BaseController
                 $this->aftermath($user_id, $goods, $num, $o_id);
             }
             if(!empty($ajax_get)){
-                echo "<script>location.href='http://wx.pinquduo.cn/order_detail.html?order_id='+$o_id+'&type=2&user_id='+$user_id </script>";
+//注释掉代码 温立涛                echo "<script>location.href='http://wx.pinquduo.cn/order_detail.html?order_id='+$o_id+'&type=2&user_id='+$user_id </script>";
                 exit;
             }
             exit(json_encode($json));
