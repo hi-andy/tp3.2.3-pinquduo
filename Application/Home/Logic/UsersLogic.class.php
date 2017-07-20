6ac7e9b23d8994aea73bea7279433d11f73d6993
jiang949676565 <949676565@qq.com>
2017-07-20 11:57:56 +0800

    每次登录都更新一次用户头像和用户名

--- Application/Home/Logic/UsersLogic.class.php
+++ Application/Home/Logic/UsersLogic.class.php
@@ -55,7 +55,15 @@ class UsersLogic extends RelationModel
                 $map['head_pic'] = CDN . "/" . $qiniu_result[0]["key"];
                 $map['test'] = 1;
                 $row = M('users')->where('user_id=' . $user['user_id'])->save($map);
+            }elseif (!empty($data['head_pic'])){
+                $map['head_pic'] = saveimage($data['head_pic']);
+                //拉取微信头像传到七牛云
+                $qiniu = new \Admin\Controller\QiniuController();
+                $qiniu_result = $qiniu->fetch($data['head_pic'], "imgbucket", time() . rand(0, 9) . ".jpg");
+                $map['head_pic'] = CDN . "/" . $qiniu_result[0]["key"];
+                $row = M('users')->where('user_id=' . $user['user_id'])->save($map);
             }
+
             if (!$user) {
                 redis("get_user_info","3");
                 //账户不存在 注册一个
@@ -75,6 +83,8 @@ class UsersLogic extends RelationModel
                 $row = M('users')->add($map);
                 $user = get_user_info($openid, 3, $oauth, $unionid);
                 $user['status'] = 1;
+            }elseif (!empty($data['nickname'])){
+                $row = M('users')->where('user_id=' . $user['user_id'])->save(array('nickname'=>$data['nickname']));
             }
             $BASE = new BaseController();
             $user['userdetails'] = $BASE->getCountUserOrder($user['user_id']);
