8e6d81be979354717cc99b6d6faaf2c487029fcc
timeing <641227924@qq.com>
2017-08-04 09:37:50 +0800

    微信消息模板增加推送新版点赞通知

--- Application/Api_2_0_2/Controller/GroupBuyController.class.php
+++ Application/Api_2_0_2/Controller/GroupBuyController.class.php
@@ -3,7 +3,8 @@ namespace Api_2_0_2\Controller;
 use Think\Controller;
 
 class GroupBuyController extends BaseController {
-    const API_URL = 'http://api.hn.pinquduo.cn/';
+    //const API_URL = 'http://api.hn.pinquduo.cn/';
+    const API_URL = 'http://test.pinquduo.cn/';
     /**
      * 关注点赞
      */
@@ -11,9 +12,10 @@ class GroupBuyController extends BaseController {
         $group_buy_id = I('groupbuyid');
         $useropenid = I('useropenid');
         $oauth = 'weixin';
+        $nickname = I('nickname');
         $group_buy_id = (int)$group_buy_id;
         if($group_buy_id<=0){
-            echo '1';
+            echo '团id非法';
             exit();
         }
         //先检测这个团id是否合法
@@ -27,27 +29,15 @@ class GroupBuyController extends BaseController {
         $result = M('group_buy')->where($where)->find();
         //查不到数据记录
         if(count($result)<=0){
-            echo '2';
+            echo '团id查不到数据记录';
             exit();
         }
         //判断该团是不是已经结束
         if($result['end_time']<time()){
-            echo '3';
+            echo '团活动已经结束';
             exit();
         }
-        //模拟微信登录
-        // Create the stream context
-        $context = stream_context_create(array(
-            'http' => array(
-                'timeout' => 5
-            )
-        ));
-        // Fetch the URL's contents
-        $url = self::API_URL."api_2_0_2/User/thirdLogin/openid/{$useropenid}/oauth/{$oauth}";
-
-        $contents = file_get_contents($url, 0, $context);
-        $getArray = json_decode($contents,true);
-        $userdata = $getArray['result'];
+        $userdata = $this->thirdLogin($useropenid,$oauth,$nickname);
         $postdata['user_id'] = $userdata['user_id'];
         $postdata['prom_id'] = $result['id'];
         $postdata['goods_id'] = $result['goods_id'];
@@ -70,12 +60,78 @@ class GroupBuyController extends BaseController {
             'content' => http_build_query($postdata, '', '&'),
         );
         $resdata = file_get_contents($posturl, false, stream_context_create($postcontext));
-
+        $resArr = json_decode($resdata,true);
         $wxtmplmsg = new WxtmplmsgController();
-        $wxtmplmsg->spell_success($useropenid,'yyyy','wwwwwww','如果未按承诺时间发货，平台将对商家进行处罚。','【VIP专享】9.9元购买（电蚊拍充电式灭蚊拍、COCO香水型洗衣液、20支软毛牙刷）');
+        $wxmsg = ((int)$resArr['status'] == -1) ? '您的好友参团失败':'您的好友参团成功';
+        $wxtmplmsg->groupbuy_msg($useropenid,$wxmsg,'助力享免单','获得0元秒杀权利');
 
         echo $resdata;
         exit();
+    }
+
+    /**
+     * 我的免单
+     */
+    public function danList(){
+        $userid = I('userid',0);
+        $userid = (int)$userid;
+        if($userid<=0){
+            $data = [
+                'status' => -1,
+                'msg' => '参数非法'
+            ];
+            $this->ajaxReturn($data,'JSON');
+            exit();
+        }
+        $uerdata = M('users')->field("user_id")->where("user_id={$userid}")->find();
+        if(count($uerdata) == 0){
+            $data = [
+                'status' => -1,
+                'msg' => '用户id无效'
+            ];
+            $this->ajaxReturn($data,'JSON');
+            exit();
+        }
+        $groupbuylist = M('group_buy')->field("id,goods_id,end_time,goods_name,is_successful,goods_num")->where("is_raise=1 and mark=0 and user_id={$userid}")->select();
+        if(count($groupbuylist)>0){
+            foreach ($groupbuylist as $k => $value){
+                $buyid = (int)$value['id'];
+                $goods_id = (int)$value['goods_id'];
+                $groupbuylist[$k]['end'] = 0;
+                if($value['end_time']<time()){
+                    $groupbuylist[$k]['end'] = 1;
+                }
+                $goodsinfo = M('goods')->field('original_img,list_img')->where('goods_id='.$goods_id)->find();
+                $groupbuylist[$k]['list_img'] = $goodsinfo['list_img'];
+                $groupbuylist[$k]['original_img'] = $goodsinfo['original_img'];
+                $cantuannum = M('group_buy')->where('mark='.$buyid)->count();
+                $cantuannum = (int)$cantuannum + 1;
+                $groupbuylist[$k]['morenum'] = (int)$value['goods_num']-$cantuannum;
+            }
+        }
+        $data = [
+            'status' => 1,
+            'msg' => '获取信息成功',
+            'list' => $groupbuylist
+        ];
+        $this->ajaxReturn($data,'JSON');
+        exit();
+
+    }
+
+
+        /*
+         * 第三方登录
+         */
+    public function thirdLogin($openid,$oauth,$nickname){
+        $map['openid'] = $openid;
+        $map['oauth'] = $oauth;
+        $map['nickname'] = $nickname;
+        $map['head_pic'] = '';
+        $map['unionid'] = '';
+        $userLogic = new \Home\Logic\UsersLogic();
+        $data = $userLogic->thirdLogin($map);
+        return $data;
     }
 
 }
