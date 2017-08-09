4a9e70c61a00489c6135da30441954d673b509cd
jiang949676565 <949676565@qq.com>
2017-08-09 14:44:56 +0800

    安能物流接入物流数据修改

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1117,3 +1117,23 @@ function curl_file_get_contents($durl){
 	curl_close($ch);
 	return $r;
 }
+
+/**
+ * 对象 转 数组
+ * 创建者 吴银海
+ * @param object $obj 对象
+ * @return array
+ */
+function object_to_array($obj) {
+	$obj = (array)$obj;
+	foreach ($obj as $k => $v) {
+		if (gettype($v) == 'resource') {
+			return;
+		}
+		if (gettype($v) == 'object' || gettype($v) == 'array') {
+			$obj[$k] = (array)object_to_array($v);
+		}
+	}
+
+	return $obj;
+}
